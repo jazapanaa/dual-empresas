d73dd9684d694f5b2489e27c64392a5fa48387c6
Jesus <jazapanaa@gmail.com>
2026-02-17 22:17:43 -0500

    eliminar registro de salida

--- app/Livewire/Warehouse/WarehouseDispatchLive.php
+++ app/Livewire/Warehouse/WarehouseDispatchLive.php
@@ -106,7 +106,7 @@ class WarehouseDispatchLive extends Component
     }
     public function deleteInventoryMovement($inventory_movement_id):void
     {
-        $this->form->delete($inventory_movement_id);
+        $this->form->delete($inventory_movement_id, $this->movement_type);
     }
     public function mount()
     {
